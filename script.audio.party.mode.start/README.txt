e330ec333be62fc6db40722c3653f1d8dabb85d4
teeedubb <teeedubb@gmail.com>
2013-12-31 14:16:43 +1100

    audio party mode 0.1.2

--- script.audio.party.mode.start/README.txt
+++ script.audio.party.mode.start/README.txt
@@ -1,1 +1,3 @@
 script.partymode
+
+Launches Music Party Mode, waits for X seconds then switches to full screen visualisation and optionally toggles the fullscreen music info after X seconds. Timeouts can be configured in addon settings, defaults are 5 seconds for each.
--- script.audio.party.mode.start/default.py
+++ script.audio.party.mode.start/default.py
@@ -1,11 +1,19 @@
-import xbmc
-import xbmcgui
+import xbmc, xbmcgui
 import xbmcaddon
 import time
 
-xbmc.executebuiltin("XBMC.PlayerControl(Stop)")
-xbmc.executebuiltin("XBMC.PlayerControl(PartyMode)")
-time.sleep(5)
-xbmc.executebuiltin("XBMC.Action(Fullscreen)")
-time.sleep(3)
-xbmc.executebuiltin("XBMC.Action(Info)")
+addon          = xbmcaddon.Addon(id='script.audio.party.mode.start')
+visTimeout     = int(addon.getSetting("visTimeout"))
+visInfoHide    = addon.getSetting("visInfoHide")
+visInfoTimeout = int(addon.getSetting("visInfoTimeout"))
+
+def startPartyMode():
+	xbmc.executebuiltin("XBMC.PlayerControl(Stop)")
+	xbmc.executebuiltin("XBMC.PlayerControl(PartyMode)")
+	time.sleep(visTimeout)
+	xbmc.executebuiltin("XBMC.Action(Fullscreen)")
+	if visInfoHide == 'true':
+		time.sleep(visInfoTimeout)
+		xbmc.executebuiltin("XBMC.Action(Info)")
+		
+startPartyMode()
