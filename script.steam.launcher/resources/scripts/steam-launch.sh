fdf7a87ffa81dee25c7751e6b9dee4121d7025b6
tw <teeedubb@gmail.com>
2014-07-07 02:17:38 +1000

    basic android support added, for shits and giggles

--- script.steam.launcher/resources/scripts/steam-launch.sh
+++ script.steam.launcher/resources/scripts/steam-launch.sh
@@ -62,6 +62,12 @@ fi
         ;;
     Linux)
 
+if [[ -f /system/build.prop ]] ; then
+  am start -n com.valvesoftware.android.steam.community/.activity.CommunityActivity
+  exit
+fi
+    
+    
 if [[ $5 != false ]] ; then
   "$5" "$3"
 fi
