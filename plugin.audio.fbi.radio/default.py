94231ae95ac9508d0001e82e4640707f905be511
mechtronsystems <32609970+mechtronsystems@users.noreply.github.com>
2021-07-27 07:33:36 +1000

    Update default.py
    change to new streaming location
    delete fbi click as no longer operating

--- plugin.audio.fbi.radio/default.py
+++ plugin.audio.fbi.radio/default.py
@@ -6,16 +6,10 @@ addonPath = addon.getAddonInfo('path')
 
 addon_handle = int(sys.argv[1])
 
-url = 'http://54.206.93.168:8000/stream'
+url = 'https://stream.fbiradio.com/stream.mp3'
 li = xbmcgui.ListItem('FBi', iconImage=os.path.join(addonPath, 'resources', 'icons', 'fbi.png'))
 li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'fbi.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'FBi'})
-li.setProperty('IsPlayable', 'True')
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-url = 'http://fbiradio.myradio.click:8000/click'
-li = xbmcgui.ListItem('FBi Click', iconImage=os.path.join(addonPath, 'resources', 'icons', 'fbi-click.png'))
-li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'fbi-click.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
-li.setInfo( 'music', { "Title": 'FBi Click'})
 li.setProperty('IsPlayable', 'True')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
 xbmcplugin.endOfDirectory(addon_handle, cacheToDisc=False)
