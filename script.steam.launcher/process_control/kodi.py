b932c57d9efd528c4c7f500d63160c077b27cc17
BdN3504 <ein_armer_schlucker@gmx.de>
2021-11-07 17:36:18 +0100

    Need to quit kodi manually, there is no other way on raspbian

--- script.steam.launcher/process_control/kodi.py
+++ script.steam.launcher/process_control/kodi.py
@@ -1,4 +1,4 @@
-from subprocess import check_output, check_call
+from subprocess import check_output
 from re import search
 from re import IGNORECASE
 
@@ -6,13 +6,13 @@ from re import IGNORECASE
 class Kodi:
 
     def __init__(self):
-        self.__pattern = r'(kodi)(\.bin|-x11|-wayland|-gbm)[_v7|_v8]*'
+        self.__bin_pattern = r'(kodi)(\.bin|-x11|-wayland|-gbm)[_v7|_v8]*'
         self.__bin_name = self.get_bin_name()
         self.__pid = self.__get_pid()
 
     def get_bin_name(self):
         running_process_names = check_output(['ps', '-eo', 'comm='])
-        return search(self.__pattern, running_process_names, IGNORECASE).group()
+        return search(self.__bin_pattern, running_process_names, IGNORECASE).group()
 
     def __get_pid(self):
         unformatted_pid = check_output(['ps', '-C', self.__bin_name, '-o', 'pid='])
