e74ea2d543a954f0fff859fdbc1772a94a2e1702
BdN3504 <ein_armer_schlucker@gmx.de>
2021-11-08 11:03:46 +0100

    Wait until kodi has quit

--- script.steam.launcher/resources/scripts/steam-link-launcher.sh
+++ script.steam.launcher/resources/scripts/steam-link-launcher.sh
@@ -13,6 +13,14 @@ $1 %u & disown
 curl -X POST -H 'Content-Type: application/json' -i http://localhost:8080/jsonrpc --data '{"jsonrpc":"2.0","id":1,"method":"Application.Quit"}'
 
 while true; do
+  sleep 5
+  if ! pgrep -x "kodi" > /dev/null
+  then
+      break
+  fi
+done
+
+while true; do
   if ! pgrep -x "steamlink-cec" > /dev/null
   then
       $2 && exit
