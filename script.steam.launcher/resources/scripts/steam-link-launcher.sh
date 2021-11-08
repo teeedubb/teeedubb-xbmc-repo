a399805726ebf30ce2a0403acef03c173921f34a
BdN3504 <ein_armer_schlucker@gmx.de>
2021-11-08 10:07:29 +0100

    Remove the @ sign from autostart, otherwise the command will be executed indefinitely

--- script.steam.launcher/resources/scripts/steam-link-launcher.sh
+++ script.steam.launcher/resources/scripts/steam-link-launcher.sh
@@ -11,3 +11,11 @@ echo "trying to run $1"
 $1 %u & disown
 
 curl -X POST -H 'Content-Type: application/json' -i http://localhost:8080/jsonrpc --data '{"jsonrpc":"2.0","id":1,"method":"Application.Quit"}'
+
+while true; do
+  if ! pgrep -x "steamlink-cec" > /dev/null
+  then
+      $2 && exit
+  fi
+  sleep 5
+done
