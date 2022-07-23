2cfc4b7f8b9117a9ec8d49a3c2d958ddabcb978f
marko <32609970+mechtronsystems@users.noreply.github.com>
2022-07-23 21:45:54 +1000

    Update default.py
    spelling

--- plugin.audio.triple.j/default.py
+++ plugin.audio.triple.j/default.py
@@ -18,7 +18,7 @@ li.setInfo( 'music', { "Title": 'Double J Online'})
 li.setProperty('IsPlayable', 'True')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
 url = 'http://abcradiolivehls-lh.akamaihd.net/i/triplejunearthed_1@327301/index_64_a-p.m3u8?sd=10&rebase=on&hdntl='
-li = xbmcgui.ListItem('Triple J Uneartherd Online', iconImage=os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'))
+li = xbmcgui.ListItem('Triple J Unearthed Online', iconImage=os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'))
 li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'Triple J Uneartherd Online'})
 li.setProperty('IsPlayable', 'True')
