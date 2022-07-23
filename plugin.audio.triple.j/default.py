8f880f418b9a8beec289072659ba887d79788c6e
marko <32609970+mechtronsystems@users.noreply.github.com>
2022-07-23 21:36:58 +1000

    Update default.py
    updated triple J url's

--- plugin.audio.triple.j/default.py
+++ plugin.audio.triple.j/default.py
@@ -5,19 +5,19 @@ addon = xbmcaddon.Addon(id='plugin.audio.triple.j')
 addonPath = addon.getAddonInfo('path')
 addon_handle = int(sys.argv[1])
 
-url = 'http://abcradiolivehls-lh.akamaihd.net/i/triplejnsw_1@327300/index_64_a-b.m3u8?sd=10&rebase=on&hdntl='
+url = 'http://streaming.c3.abc.net.au/hottest100_a_aac'
 li = xbmcgui.ListItem('Triple J Online', iconImage=os.path.join(addonPath, 'resources', 'icons', 'triplej.png'))
 li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'triplej.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'Triple J Online'})
 li.setProperty('IsPlayable', 'True')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-url = 'http://abcradiolivehls-lh.akamaihd.net/i/doublejnsw_1@327293/index_64_a-b.m3u8?sd=10&rebase=on&hdntl='
+url = 'http://live-radio01.mediahubaustralia.com/DJDW/aac/'
 li = xbmcgui.ListItem('Double J Online', iconImage=os.path.join(addonPath, 'resources', 'icons', 'doublej.png'))
 li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'doublej.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'Double J Online'})
 li.setProperty('IsPlayable', 'True')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-url = 'http://abcradiolivehls-lh.akamaihd.net/i/triplejunearthed_1@327301/index_64_a-p.m3u8?sd=10&rebase=on&hdntl='
+url = 'http://live-radio01.mediahubaustralia.com/UNEW/aac/'
 li = xbmcgui.ListItem('Triple J Uneartherd Online', iconImage=os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'))
 li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'Triple J Uneartherd Online'})
