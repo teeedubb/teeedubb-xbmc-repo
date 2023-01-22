dd7f7641c2597b6db3f95e948ae43807c7667ba5
teeedubb <teeedubb@gmail.com>
2023-01-22 22:43:50 +1100

    update for kodi 19 and update stream url

--- plugin.audio.fbi.radio/default.py
+++ plugin.audio.fbi.radio/default.py
@@ -6,9 +6,9 @@ addonPath = addon.getAddonInfo('path')
 
 addon_handle = int(sys.argv[1])
 
-url = 'http://streamer.fbiradio.com:8000/stream'
-li = xbmcgui.ListItem('FBi', iconImage=os.path.join(addonPath, 'resources', 'icons', 'fbi.png'))
-li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'fbi.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
+url = 'https://stream.fbiradio.com/stream'
+li = xbmcgui.ListItem('FBi')
+li.setArt({ 'icon': os.path.join(addonPath, 'resources', 'icons', 'fbi.png'), 'thumb': os.path.join(addonPath, 'resources', 'icons', 'fbi.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'FBi'})
 li.setProperty('IsPlayable', 'True')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
