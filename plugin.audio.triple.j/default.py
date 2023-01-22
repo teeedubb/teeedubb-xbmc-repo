a22c13ed5a19007b85c00a3bcb97d2800517220b
teeedubb <teeedubb@gmail.com>
2023-01-22 19:18:03 +1100

    Update URLs

--- plugin.audio.triple.j/default.py
+++ plugin.audio.triple.j/default.py
@@ -5,21 +5,21 @@ addon = xbmcaddon.Addon(id='plugin.audio.triple.j')
 addonPath = addon.getAddonInfo('path')
 addon_handle = int(sys.argv[1])
 
-url = 'http://abcradiolivehls-lh.akamaihd.net/i/triplejnsw_1@327300/index_64_a-b.m3u8?sd=10&rebase=on&hdntl='
-li = xbmcgui.ListItem('Triple J Online', iconImage=os.path.join(addonPath, 'resources', 'icons', 'triplej.png'))
-li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'triplej.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
+url = 'https://mediaserviceslive.akamaized.net/hls/live/2038308/triplejnsw/index.m3u8'
+li = xbmcgui.ListItem('Triple J Online')
+li.setArt({ 'icon': os.path.join(addonPath, 'resources', 'icons', 'triplej.png'), 'thumb': os.path.join(addonPath, 'resources', 'icons', 'triplej.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'Triple J Online'})
 li.setProperty('IsPlayable', 'True')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-url = 'http://abcradiolivehls-lh.akamaihd.net/i/doublejnsw_1@327293/index_64_a-b.m3u8?sd=10&rebase=on&hdntl='
-li = xbmcgui.ListItem('Double J Online', iconImage=os.path.join(addonPath, 'resources', 'icons', 'doublej.png'))
-li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'doublej.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
+url = 'https://mediaserviceslive.akamaized.net/hls/live/2038315/doublejnsw/index.m3u8'
+li = xbmcgui.ListItem('Double J Online')
+li.setArt({ 'icon': os.path.join(addonPath, 'resources', 'icons', 'doublej.png'), 'thumb': os.path.join(addonPath, 'resources', 'icons', 'doublej.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'Double J Online'})
 li.setProperty('IsPlayable', 'True')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-url = 'http://abcradiolivehls-lh.akamaihd.net/i/triplejunearthed_1@327301/index_64_a-p.m3u8?sd=10&rebase=on&hdntl='
-li = xbmcgui.ListItem('Triple J Uneartherd Online', iconImage=os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'))
-li.setArt({ 'thumb': os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
+url = 'https://mediaserviceslive.akamaized.net/hls/live/2038305/triplejunearthed/index.m3u8'
+li = xbmcgui.ListItem('Triple J Uneartherd Online')
+li.setArt({ 'icon': os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'), 'thumb': os.path.join(addonPath, 'resources', 'icons', 'unearthed.png'), 'fanart': os.path.join(addonPath, 'fanart.jpg') })
 li.setInfo( 'music', { "Title": 'Triple J Uneartherd Online'})
 li.setProperty('IsPlayable', 'True')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
