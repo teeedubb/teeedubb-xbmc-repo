36b5d29ca8697855acb3d4c9c92abc32da4ade4e
teeedubb <teeedubb@gmail.com>
2023-02-21 20:24:13 +1100

    script.audio.party.mode.start-1.0.0

--- script.audio.party.mode.start/default.py
+++ script.audio.party.mode.start/default.py
@@ -9,12 +9,12 @@ visInfoHide = addon.getSetting("visInfoHide")
 visInfoTimeout = int(addon.getSetting("visInfoTimeout"))
 
 def startPartyMode():
-	xbmc.executebuiltin("XBMC.PlayerControl(Stop)")
-	xbmc.executebuiltin("XBMC.PlayerControl(PartyMode)")
+	xbmc.executebuiltin("PlayerControl(Stop)")
+	xbmc.executebuiltin("PlayerControl(PartyMode)")
 	time.sleep(visTimeout)
-	xbmc.executebuiltin("XBMC.Action(Fullscreen)")
+	xbmc.executebuiltin("Action(Fullscreen)")
 	if visInfoHide == 'true':
 		time.sleep(visInfoTimeout)
-		xbmc.executebuiltin("XBMC.Action(Info)")
+		xbmc.executebuiltin("Action(Info)")
 		
 startPartyMode()
